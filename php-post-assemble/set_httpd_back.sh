de82e690ef79921839b26e31f659bdee140d3eab
Stephen Cuppett <steve@cuppett.com>
2021-08-20 19:29:19 -0400

    Bumping Nextcloud to 22.1.0

--- php-post-assemble/set_httpd_back.sh
+++ php-post-assemble/set_httpd_back.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -e
-
 # Restoring the main directory reference to /var/www/html
 sed -i "s%^<Directory \"${APP_DATA}\"%<Directory \"${HTTPD_DATA_ORIG_PATH}/html\"%" ${HTTPD_MAIN_CONF_PATH}/httpd.conf
